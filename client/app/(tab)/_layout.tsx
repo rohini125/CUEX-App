ca580642f2d5d64aacd0a1f282531015bc9a871d
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2024-12-30 22:25:35 +0530

    modify file

--- client/app/(tab)/_layout.tsx
+++ client/app/(tab)/_layout.tsx
@@ -9,28 +9,37 @@ const TabRoot = () =>{
             screenOptions={{
                 tabBarActiveTintColor: 'blue', // Active tab icon color
                 tabBarInactiveTintColor: 'gray', // Inactive tab icon color
-                tabBarStyle: { backgroundColor: 'white' }, // Optional: Tab bar background color
+                tabBarStyle: { backgroundColor: 'white' ,}, // Optional: Tab bar background color
+                headerShown: false,
+                
             }}
+            
         >
            <Tabs.Screen 
               name="front" 
-              options={{title:"Home",  
-              tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />}} />
+              options={{title:" Home ",  
+              tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />
+            }} />
             <Tabs.Screen
                 name="market"
-                options={{title:"Market",
+                options={{title:" Market ",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name="line-chart" color={color} /> 
-             }}/>
+            }}/>
+            <Tabs.Screen 
+                name="scanner"
+                options={{title:"Scanner",
+                    
+            }}/>
             <Tabs.Screen 
                 name="wallet"
-                options={{title:"Wallet",
+                options={{title:" Wallet ",
                 tabBarIcon: ({ color }) => <FontAwesome5 size={28} name="wallet" color={color} /> 
              }}/>
             <Tabs.Screen
              name="history"
-             options={{title:"History",
-             tabBarIcon: ({ color }) => <FontAwesome size={28} name="history" color={color} />}}
-            />
+             options={{title:" History ",
+             tabBarIcon: ({ color }) => <FontAwesome size={28} name="history" color={color} />
+            }}/>
         </Tabs>
     );
 };
