8ffeb54db4875815316a53e06cdcda8fe378ea89
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2025-01-18 00:23:32 +0530

    modify file

--- client/app/(tab)/_layout.tsx
+++ client/app/(tab)/_layout.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import {StyleSheet} from 'react-native';
 import { Tabs } from "expo-router";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 ,MaterialIcons} from '@expo/vector-icons';
 
 const TabRoot = () =>{
     return (
         <Tabs 
             screenOptions={{
-                tabBarActiveTintColor: 'blue', // Active tab icon color
+                tabBarActiveTintColor: 'black', // Active tab icon color
                 tabBarInactiveTintColor: 'gray', // Inactive tab icon color
                 tabBarStyle: { backgroundColor: 'white' ,}, // Optional: Tab bar background color
                 headerShown: false,
@@ -25,11 +26,24 @@ const TabRoot = () =>{
                 options={{title:" Market ",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name="line-chart" color={color} /> 
             }}/>
-            <Tabs.Screen 
-                name="scanner"
-                options={{title:"Scanner",
-                    
+
+            <Tabs.Screen
+                    name="scanner"
+                    options={{
+                    title: " ",
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name="qr-code-scanner" size={28} color={color} style={styles.scanner}/>
+                    ),
             }}/>
+            {/* <Tabs.Screen 
+                name="scanner"
+                options={{title:" ",
+                // tabBarIcon: ({ color }) => <FontAwesome size={28} name="qrcode" color={color} /> 
+                tabBarIcon: ({ color }) => (
+                    <MaterialIcons name="qr-code-scanner" size={28} color={color} /> // QR code scanner icon
+                  ),
+                tabBarStyle: { backgroundColor: 'pink' }
+            }}/> */}
             <Tabs.Screen 
                 name="wallet"
                 options={{title:" Wallet ",
@@ -45,3 +59,13 @@ const TabRoot = () =>{
 };
 
 export default TabRoot;
+
+const styles=StyleSheet.create({
+    scanner:{
+        backgroundColor:'blue',
+        color:'white',
+        padding:8,
+        borderRadius:"50%",
+        marginBottom:25,
+    },
+});
